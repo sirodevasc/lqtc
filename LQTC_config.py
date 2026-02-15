9a0cb36018fe67ca6cad1c42989d979c0355a523
siro <peterpinge@aol.com>
2026-02-15 15:33:26 +0100

    Add files via upload

---
+++ LQTC_config.py
@@ -0,0 +1,13 @@
+#
+# list of data to import#
+#
+# from version 1.52
+#
+TheToken, TokenPair = '0x', '0x'
+#
+TGtoken, TGchat_id = 'xx:xx', 'xxx'
+#
+mywallet2, mykey2 = '0x', ''
+mywallet3, mykey3 = '0x', ''
+#
+#
